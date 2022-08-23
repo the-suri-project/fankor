6fc234236c0f5b1fbd2fee827e23e0f250e308df
Julio Treviño Páez <julio.tpaez@gmail.com>
2022-08-23 12:18:19 +0200

    Fix Fankor.toml reading when used under workspace

--- framework/fankor-syn/src/fankor/mod.rs
+++ framework/fankor-syn/src/fankor/mod.rs
@@ -1,21 +1,37 @@
 pub use config::*;
+use std::env;
+use std::path::PathBuf;
+
 mod config;
 
 /// Reads the `Fankor.toml` file.
 pub fn read_fankor_toml() -> FankorConfig {
     // Read config.
-    let config = match std::fs::read_to_string("./Fankor.toml") {
+    let manifest_dir =
+        env::var("CARGO_MANIFEST_DIR").expect("Could not find `CARGO_MANIFEST_DIR` env variable.");
+
+    let fankor_toml_path = PathBuf::from(manifest_dir).join("Fankor.toml");
+
+    let config = match std::fs::read_to_string(fankor_toml_path.as_path()) {
         Ok(file_content) => match toml::from_str::<FankorConfig>(file_content.as_str()) {
             Ok(mut config) => {
                 config.fill_with_defaults();
                 config
             }
             Err(e) => {
-                panic!("ERROR: Failed to parse Fankor.toml: {}", e);
+                panic!(
+                    "ERROR: Failed to parse Fankor.toml at {:?}: {}",
+                    fankor_toml_path.as_os_str(),
+                    e
+                );
             }
         },
         Err(e) => {
-            panic!("ERROR: missing Fankor.toml: {}", e);
+            panic!(
+                "ERROR: missing Fankor.toml at {:?}: {}",
+                fankor_toml_path.as_os_str(),
+                e
+            );
         }
     };
 
