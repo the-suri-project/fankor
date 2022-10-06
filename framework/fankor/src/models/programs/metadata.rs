571ce3b5d17cc58c18cf6ce0a16a07f1dd4d122a
Julio Treviño Páez <julio.tpaez@gmail.com>
2022-10-06 09:07:52 +0200

    Bump dependencies and solve some clippy bugs

--- framework/fankor/src/models/programs/metadata.rs
+++ framework/fankor/src/models/programs/metadata.rs
@@ -29,61 +29,70 @@ impl_account!(
     mpl_token_metadata::state::Metadata,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
 impl_account!(
     MasterEditionV1,
     mpl_token_metadata::state::MasterEditionV1,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
 impl_account!(
     MasterEditionV2,
     mpl_token_metadata::state::MasterEditionV2,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
 impl_account!(
     Edition,
     mpl_token_metadata::state::Edition,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
 impl_account!(
     ReservationListV1,
     mpl_token_metadata::state::ReservationListV1,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
 impl_account!(
     ReservationListV2,
     mpl_token_metadata::state::ReservationListV2,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
 impl_account!(
     EditionMarker,
     mpl_token_metadata::state::EditionMarker,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
 impl_account!(
     UseAuthorityRecord,
     mpl_token_metadata::state::UseAuthorityRecord,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
 impl_account!(
     CollectionAuthorityRecord,
     mpl_token_metadata::state::CollectionAuthorityRecord,
     &mpl_token_metadata::ID,
     deserialize,
-    safe_deserialize
+    safe_deserialize,
+    [Eq]
 );
