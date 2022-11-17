18c9bb1d77ea7aba36468a6a0ac2b24adf6823c4
Julio Treviño Páez <julio@treviware.com>
2022-11-17 17:42:38 +0100

    Add remaining associated token account CPI methods

---
+++ framework/fankor/src/cpi/associated_token/mod.rs
@@ -0,0 +1,7 @@
+pub use create_associated_token_account::*;
+pub use create_associated_token_account_idempotent::*;
+pub use recover_nested::*;
+
+mod create_associated_token_account;
+mod create_associated_token_account_idempotent;
+mod recover_nested;
---
+++ framework/fankor/src/cpi/associated_token/recover_nested.rs
@@ -0,0 +1,36 @@
+use crate::errors::Error;
+use crate::models::{AssociatedToken, Program};
+use crate::prelude::FankorResult;
+use solana_program::account_info::AccountInfo;
+
+pub struct CpiRecoverNested<'info> {
+    pub wallet_address: AccountInfo<'info>,
+    pub owner_token_mint_address: AccountInfo<'info>,
+    pub nested_token_mint_address: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+}
+
+pub fn recover_nested(
+    _program: &Program<AssociatedToken>,
+    accounts: CpiRecoverNested,
+    signer_seeds: &[&[&[u8]]],
+) -> FankorResult<()> {
+    let ix = spl_associated_token_account::instruction::recover_nested(
+        accounts.wallet_address.key,
+        accounts.owner_token_mint_address.key,
+        accounts.nested_token_mint_address.key,
+        accounts.token_program.key,
+    );
+
+    solana_program::program::invoke_signed(
+        &ix,
+        &[
+            accounts.wallet_address,
+            accounts.owner_token_mint_address,
+            accounts.nested_token_mint_address,
+            accounts.token_program,
+        ],
+        signer_seeds,
+    )
+    .map_or_else(|e| Err(Error::ProgramError(e)), |_| Ok(()))
+}
