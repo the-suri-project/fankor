9107ca53785fb5fa26170b66114340aa5e4fad24
Julio Treviño Páez <julio@treviware.com>
2023-03-14 13:26:51 +0100

    Fix clone method in TS

--- ts-client/src/utils/clone.ts
+++ ts-client/src/utils/clone.ts
@@ -1,4 +1,4 @@
-import { Keypair, PublicKey } from '@solana/web3.js';
+import { PublicKey } from '@solana/web3.js';
 
 export function clone(v: any): any {
     switch (typeof v) {
@@ -22,16 +22,16 @@ export function clone(v: any): any {
         return v.slice();
     }
 
-    if (v instanceof Keypair || v instanceof PublicKey) {
+    if (v.map) {
+        return v.map(clone);
+    }
+
+    if (v instanceof PublicKey || isPublicKey(v)) {
         return v;
     }
 
     if ((v as any).clone) {
         return v.clone();
-    }
-
-    if (Array.isArray(v)) {
-        return v.map(clone);
     }
 
     const result: any = {};
@@ -40,4 +40,26 @@ export function clone(v: any): any {
     }
 
     return result;
+}
+
+function isPublicKey(v: any): v is PublicKey {
+    if (v instanceof PublicKey) {
+        return true;
+    }
+
+    const prototype: any = Object.getPrototypeOf(v);
+
+    return (
+        typeof prototype?.constructor === 'function' &&
+        typeof prototype?.toBase58 === 'function' &&
+        typeof prototype?.toBuffer === 'function' &&
+        typeof prototype?.equals === 'function' &&
+        typeof prototype?.toBytes === 'function' &&
+        typeof prototype?.toJSON === 'function' &&
+        typeof prototype?.constructor?.createProgramAddress === 'function' &&
+        typeof prototype?.constructor?.createProgramAddressSync ===
+            'function' &&
+        typeof prototype?.constructor?.findProgramAddress === 'function' &&
+        typeof prototype?.constructor?.findProgramAddressSync === 'function'
+    );
 }
