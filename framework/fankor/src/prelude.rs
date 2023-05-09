40f751e72245c52f9eb87e8406127bfdbe9c647f
Julio Treviño Páez <julio@treviware.com>
2023-05-09 08:27:50 +0200

    Export token 2022 in prelude

--- framework/fankor/src/prelude.rs
+++ framework/fankor/src/prelude.rs
@@ -1,22 +1,9 @@
-pub use crate::cpi;
-pub use crate::errors::*;
-pub use crate::macros::*;
-pub use crate::models::types::*;
-pub use crate::models::*;
-#[cfg(feature = "testable-program")]
-pub use crate::testable_program::*;
-pub use crate::traits::*;
-#[cfg(feature = "ts-gen")]
-pub use crate::ts_gen;
-pub use crate::utils::seeds::byte_seeds_to_slices;
-pub use crate::utils::type_id_of;
-pub use crate::utils::writers::ArrayWriter;
-pub use crate::utils::writers::VecWriter;
 pub use borsh;
 pub use borsh::BorshDeserialize;
 pub use borsh::BorshSerialize;
 pub use bs58;
-pub use fankor_macros::*;
+#[cfg(feature = "ts-gen")]
+pub use lazy_static::lazy_static;
 #[cfg(feature = "metadata-program")]
 pub use mpl_token_metadata;
 pub use solana_program;
@@ -33,24 +20,40 @@ pub use solana_program::sysvar::rewards::Rewards;
 pub use solana_program::sysvar::slot_hashes::SlotHashes;
 pub use solana_program::sysvar::slot_history::SlotHistory;
 pub use solana_program::sysvar::stake_history::StakeHistory;
-#[cfg(not(feature = "no-entrypoint"))]
-pub use solana_security_txt::security_txt;
-#[cfg(feature = "token-program")]
-pub use spl_associated_token_account;
-#[cfg(feature = "token-program")]
-pub use spl_token;
-pub use static_assertions::const_assert;
-
-#[cfg(feature = "test-utils")]
-pub use crate::tests::*;
-#[cfg(feature = "ts-gen")]
-pub use lazy_static::lazy_static;
 #[cfg(feature = "test-utils")]
 pub use solana_program_runtime;
 #[cfg(feature = "test-utils")]
 pub use solana_program_test;
 #[cfg(feature = "test-utils")]
 pub use solana_sdk;
+#[cfg(not(feature = "no-entrypoint"))]
+pub use solana_security_txt::security_txt;
+#[cfg(feature = "token-program")]
+pub use spl_associated_token_account;
+#[cfg(feature = "token-program")]
+pub use spl_token;
+#[cfg(feature = "token-program-2022")]
+pub use spl_token_2022;
+pub use static_assertions::const_assert;
+
+pub use fankor_macros::*;
+
+pub use crate::cpi;
+pub use crate::errors::*;
+pub use crate::macros::*;
+pub use crate::models::types::*;
+pub use crate::models::*;
+#[cfg(feature = "testable-program")]
+pub use crate::testable_program::*;
+#[cfg(feature = "test-utils")]
+pub use crate::tests::*;
+pub use crate::traits::*;
+#[cfg(feature = "ts-gen")]
+pub use crate::ts_gen;
+pub use crate::utils::seeds::byte_seeds_to_slices;
+pub use crate::utils::type_id_of;
+pub use crate::utils::writers::ArrayWriter;
+pub use crate::utils::writers::VecWriter;
 
 pub mod sysvar {
     pub use solana_program::sysvar::*;
